71e553643f4eaa41155eb14f04268c7662612558
stivenrivera <ferofern@espol.edu.ec>
2025-01-15 14:57:02 -0500

    Update OperationsTest.java

--- src/test/java/calculatorsmp/OperationsTest.java
+++ src/test/java/calculatorsmp/OperationsTest.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author CltControl
@@ -49,6 +52,50 @@ public class OperationsTest {
         fail("The test case is a prototype.");
     }
 
+
+        @Test
+    public void testMakeFormulaNumbersInRange() {
+        String formula = Operations.MakeFormula();
+        String[] numbers = formula.split("[\\+\\-\\*/]");
+        System.out.println("Todos los numeros deben estar entre 1-99");
+        for(String s : numbers){
+            int number = Integer.parseInt(s);
+            assertTrue(number>=1 && number<= 99, "Numero fuera de rango: " + number);
+        }
+    }
+    
+    @Test
+    public void testMakeFormulaNumberOfOperations(){
+        String formula = Operations.MakeFormula();
+        System.out.println(formula);
+        int operations = formula.replaceAll("[\\+\\-\\*/]", "").length();
+        assertTrue(operations >= 1 && operations <= 8, "Operadores fuera de rango:" + operations);
+    }
+    
+    @Test
+    public void testMakeFormulaRandomness(){
+        Set<String> formulas = new HashSet<>();
+        for(int i = 0; i < 100; i++){
+            formulas.add((Operations.MakeFormula()));
+        }
+        assertTrue(formulas.size() > 1, "Las formulas no son aleatorias");
+    }
+    
+    @Test
+    public void testMakeFormulaStructure(){
+        String formula = Operations.MakeFormula();
+        assertTrue(formula.matches("([1-9][0-9]?|99)([\\+\\-\\*/])([1-9][0-9]?|99)"), "Estructura invalida: "+formula);
+        assertFalse(formula.contains("++") || formula.contains("--") || formula.contains("**") || formula.contains("//"),
+                "Formula contiene operadores consecutivos: "+formula);
+    }
+    
+    @Test
+    public void testMakeFormulaNoSpaces(){
+        String formula = Operations.MakeFormula();
+        
+        assertTrue(formula.contains(" "), "La formula no debe contener espacios: "+ formula);
+        assertFalse(formula.matches("[0-9+\\-\\*/]"), "La formla contiene caracteres invalidos: "+formula);
+    }
     /**
      * Test of Solve method, of class Operations.
      */
